33c3ebcec55f85aaec1c628b2fb5b6b05a4735a6
unknown <muktinathrajbanshi786@gmail.com>
2026-02-10 09:47:10 +0545

    Some important data is notted

---
+++ types/type_annotation.ts
@@ -0,0 +1,31 @@
+//? Introduction to Types in TypeScript - P-4
+// * In TypeScript, type annotation is a way of explicitly specifying the type of a variable,
+// * function parameter, or function return value.
+
+//! Advantages
+//? It helps the TypeScript compiler to enforce type checking and provide type safety during
+//?  development.
+//? What operations could we performed on that variable value
+//? Type annotations are expressed by using a colon (:) followed by the desired type after the variable
+//? or function parameter declaration. There can be a space after the clone.
+
+// * -------------------------------------------------------
+// * Number Type
+// * -------------------------------------------------------
+
+//? The number type represents numeric values, including integers and floating-point numbers.
+
+//! TODO Arithmetic Operations:
+//? Number types in TypeScript support arithmetic operations such as 
+//? addition (+), subtraction (-), multiplication (*), and division (/).
+//? The result of these operations on numbers is also of type number.
+
+//! Here are some practice questions related to string types iin TypeScript:
+
+//* todo 👉 String Initialization:
+//? Declare a variable message of type string and assign it the value "Hello, TypeScript!."
+
+//* todo 👉 Concatenation:
+//? Create two variables firstName and lastName of type string and assign them your first name and 
+//? last name, respectively. Concatenate the two variables and store result in a variable called 
+//? fullName.
