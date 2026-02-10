5cd16bacf0dbe27b0593624e5db3d6d9adc4b6fc
unknown <muktinathrajbanshi786@gmail.com>
2026-02-10 09:19:53 +0545

    Deep Dive into TS Code, Catching Errors & Solving & TS Config File

--- index.js
+++
@@ -1,3 +0,0 @@
-console.log("I am Muktinath Rajbanshi");
-var num = 5;
-num = 55;
--- index.ts
+++ index.ts
@@ -1,5 +1,17 @@
-console.log("I am Muktinath Rajbanshi");
+// console.log("I am Muktinath Rajbanshi");
+// let num:number = 5
+// num = 55;
 
-let num:number = 5
-num = 55;
+//? TODO 1: Write a program to add two numbers?
+//? TODO 2: How to catch Errors & solve it?
+//? TODO 3: TS Configuration File
 
+//* func define
+//@ts-ignore
+const sum = ( a:number , b:number) :number => {
+    return a + b
+}
+
+
+//* fun call 
+console.log(sum(5, "Muktinath"));
