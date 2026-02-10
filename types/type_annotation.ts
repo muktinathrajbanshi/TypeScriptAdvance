e0b75fa8871a2b66379e3767fe97da75ee3c504c
unknown <muktinathrajbanshi786@gmail.com>
2026-02-10 10:45:03 +0545

    wow now it's awesome for the initial info

--- types/type_annotation.ts
+++ types/type_annotation.ts
@@ -40,3 +40,16 @@
 //? convert the entire sentence to lowercase and store the result in a variable called lowercaseText
 
 
+//! Home Work Time 🙅‍♂️
+// todo 👉 Substring:
+//? Declare a variable longText of type string and assign it a long sentence Extract the first 
+//? 10 characters from longText and store them in a variable called shortText.
+
+// todo 👉 String Comparison:
+//? Declare two variables str1 and str2 of type string and assign them different sentences. 
+//? Compare the two strings and store the result (true or false) in a variable called areEqual.
+
+// todo 👉 String Template:
+//? Declare variables product and price of type string and number, respectively. Create a string 
+//? using template literals to display the product and its price in the format "The product {product}
+//? is priced at {price} dollars. "
