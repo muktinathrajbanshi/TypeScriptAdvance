3a4e9f2588043f252e9a510a8ce82e8f83316375
unknown <muktinathrajbanshi786@gmail.com>
2026-02-11 09:20:20 +0545

    wow boolean assignment is completed re baba

--- types/bool_bigint.js
+++ types/bool_bigint.js
@@ -10,9 +10,13 @@
 //! Question: Check Even Number
 //* 👉 Write a TypeScript function called isEven that takes a number as a parameter and 
 //* returns true if the number is even and false otherwise.
-var isEven = function (num) {
-    return num % 2 === 0;
-};
-console.log(isEven(5));
+// const isEven = (num:number):boolean => {
+//    return num % 2 === 0;
+// }
+// console.log(isEven(5));
 //* todo write a TypeScript function called isDivisibleBy4And8 that takes a number as a parameter
 //* returns true if the number is divisible by both 4 and 8.
+var isDivisibleBy4And8 = function (num1) {
+    return num1 % 4 === 0 && num1 % 8 === 0;
+};
+console.log(isDivisibleBy4And8(16));
--- types/bool_bigint.ts
+++ types/bool_bigint.ts
@@ -17,13 +17,19 @@
 //* 👉 Write a TypeScript function called isEven that takes a number as a parameter and 
 //* returns true if the number is even and false otherwise.
 
-const isEven = (num:number):boolean => {
-   return num % 2 === 0;
-}
+// const isEven = (num:number):boolean => {
+//    return num % 2 === 0;
+// }
 
-console.log(isEven(5));
+// console.log(isEven(5));
 
 
 
 //* todo write a TypeScript function called isDivisibleBy4And8 that takes a number as a parameter
 //* returns true if the number is divisible by both 4 and 8.
+
+const isDivisibleBy4And8 = (num1:number):boolean => {
+    return num1 % 4 === 0 && num1 % 8 === 0;
+}
+
+console.log(isDivisibleBy4And8(16));
