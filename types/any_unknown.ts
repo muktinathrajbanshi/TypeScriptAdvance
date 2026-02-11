75b1e53ff7a36bf1b46af02f7979e60aa89f1edf
unknown <muktinathrajbanshi786@gmail.com>
2026-02-11 11:58:25 +0545

    aja lai yati garum hola

---
+++ types/any_unknown.ts
@@ -0,0 +1,20 @@
+// * ------------------------------------
+// * Any Type
+// * ------------------------------------
+
+//? The any type is the most flexible type in TypeScript. It essentially turns off all type checking
+//? for the variables or expressions it is applied to.
+//? you will see any type when u first write code
+
+// let myfavNum:any = 5;
+// myfavNum = "muktinath"
+// myfavNum = true
+
+//! useCases
+// * Working with Dynamic Data: When dealing with data from dynamic sources like user inputs,
+// * network responses, or deserialized JSON objects, the any type can be useful.
+
+// * Migration from JavaScript: When migrating an existing JavaScript codebase to TypeScript,
+// * using the any type can be a convenient way to quickly annotate variables and functions without
+// * immediately specifying their precise type.
+
