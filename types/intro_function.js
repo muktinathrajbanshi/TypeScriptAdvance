c811aecd89ced1ac91bf6ed804e20ff1f83362af
unknown <muktinathrajbanshi786@gmail.com>
2026-02-12 21:19:40 +0545

    wow that's awesome man

--- types/intro_function.js
+++ types/intro_function.js
@@ -2,39 +2,28 @@
 // * You can declare a function using the function keyword, followed by the function name, a list of
 // * parameters enclosed in parentheses, and a return type. The function body contains the code that
 // * will be executed when the function is called.
-
 //! Function Invocation:
 // * To execute a function, you simply invoke it by using its name followed by parentheses.
 // * You can provide arguments (actual values) for the parameters defined in the function declaration.
-
 // // Greeting function define
 // function greet(name:string, id:number) {
 //     console.log(`Welcome, ${name} and your id is ${id}`);
-    
 // }
-
 // // function call 
 // greet("muktinath", 1);
-
-
 // // Greeting function define
 // const greet = (name:string, id:number) => {
 //     console.log(`Welcome, ${name} and your id is ${id}`);
-    
 // }
 // // function call 
 // greet("muktinath", 1);
-
 //! Function return: How do you specify the return type of a function in TypeScript?
 // * To specify the return type of a function, you can use the colon (:) followed by the desired type
 // * after the function's parameter list.
-
 // Greeting function define
-const greet = (name:string, id:number):string => {
-   return `Welcome, ${name} and your id is ${id}`
-    
-}
+var greet = function (name, id) {
+    return "Welcome, ".concat(name, " and your id is ").concat(id);
+};
 // function call 
-const myGreet = greet("muktinath",1);
+var myGreet = greet("muktinath", 1);
 console.log(myGreet);
-
