a9dbc39f891c10853006729f9292aa8551838d5b
unknown <muktinathrajbanshi786@gmail.com>
2026-02-12 20:57:56 +0545

    sorry today is getting late but it's okay it's my job i have to do it any now no matter what the situation has

---
+++ types/intro_function.ts
@@ -0,0 +1,9 @@
+//! Function Declaration:
+// * You can declare a function using the function keyword, followed by the function name, a list of
+// * parameters enclosed in parentheses, and a return type. The function body contains the code that
+// * will be executed when the function is called.
+
+//! Function Invocation:
+// * To execute a function, you simply invoke it by using its name followed by parentheses.
+// * You can provide arguments (actual values) for the parameters defined in the function declaration.
+
