6ef748d4a948030c831c4b279b9dab1dd3a59c4c
unknown <muktinathrajbanshi786@gmail.com>
2026-02-13 11:04:07 +0545

    Understanding Optional and Default Parameters in TypeScript Functions

--- types/function_default_optional.js
+++ types/function_default_optional.js
@@ -2,8 +2,24 @@
 // * TypeScript allows you to define optional and default parameters in functions.
 // * Optional parameters are denoted by appending a ? symbol after the parameter name, and default
 // * parameters are specified by providing a default value in the parameter declaration.
+// const greet3 = (name:string, id:number): string => {
+//     return `Welcome, ${name} and your id is ${id}`
+// }
+// const employee = greet3("muktinath", 5);
+// console.log(employee);
+// // fun define 
+// const greet3 = (name:string, id:number = 1): string => {
+//     return `Welcome, ${name} and your id is ${id}`
+// }
 var greet3 = function (name, id) {
-    return "Welcome, ".concat(name, " and your id is ").concat(id);
+    if (id) {
+        return "Welcome, ".concat(name, " and your id is ").concat(id);
+    }
+    else {
+        return "Welcome, ".concat(name, " ");
+    }
 };
-var employee = greet3("muktinath", 5);
+var employee = greet3("muktinath");
+var employee2 = greet3("milan", 2);
 console.log(employee);
+console.log(employee2);
--- types/function_default_optional.ts
+++ types/function_default_optional.ts
@@ -1,16 +1,36 @@
 //? Optional and Default Parameters:
 
 // * TypeScript allows you to define optional and default parameters in functions.
+
 // * Optional parameters are denoted by appending a ? symbol after the parameter name, and default
 // * parameters are specified by providing a default value in the parameter declaration.
 
-const greet3 = (name:string, id:number): string => {
+// const greet3 = (name:string, id:number): string => {
+//     return `Welcome, ${name} and your id is ${id}`
+// }
+// const employee = greet3("muktinath", 5);
+// console.log(employee);
 
-    return `Welcome, ${name} and your id is ${id}`
+// // fun define 
+// const greet3 = (name:string, id:number = 1): string => {
+//     return `Welcome, ${name} and your id is ${id}`
+// }
 
+const greet3 = (name:string, id?:number): string => {
+
+    if (id) {
+        return `Welcome, ${name} and your id is ${id}`;
+    } else {
+        return `Welcome, ${name} `
+    }
 }
 
-const employee = greet3("muktinath", 5);
+// func call 
+const employee = greet3("muktinath");
+const employee2 = greet3("milan", 2);
+
 console.log(employee);
+console.log(employee2);
+
 
 
