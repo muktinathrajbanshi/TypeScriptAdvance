0a46baf126dd1944a665501198864d3db19d538c
unknown <muktinathrajbanshi786@gmail.com>
2026-02-13 19:34:59 +0545

    let's get started with array in TypeScript

---
+++ src/10_intro_arrays.ts
@@ -0,0 +1,21 @@
+//! Array in TS
+
+//? In TypeScript, you can create and initialize arrays using various approaches
+
+// a) Using square brackets:
+// b) Using the Array constructor:
+// c) Using the Array.of method:
+
+//Operation: Accessing elements using index:
+
+//TODO HomeWork Time
+const value1 = [1, 2, 3, 4, 5];
+// Is value1 an array? Yes or No?
+const value2 = "Hello, world!";
+// Is value2 an array? Yes or No?
+const value3 = { name: "John", age: 30 };
+// Is value3 an array? Yes or No?
+const value4 = [true, false, true];
+// Is value4 an array? Yes or No?
+const value5 = 42;
+// Is value5 an array? Yes or No?
