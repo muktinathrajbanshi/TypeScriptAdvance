cf2d2820dfe7ef05bc56cdc1ada0d79ac39f96a8
unknown <muktinathrajbanshi786@gmail.com>
2026-02-13 10:11:15 +0545

    some questions are attached

--- types/typeinference.ts
+++ types/typeinference.ts
@@ -6,9 +6,24 @@ let myName = "muktinath";
 let myFavNum = 5;
 // myFavNum = true
 
+let firstName = "muktinath"; // The compiler infers the type string for the variable name.
+
+let age = 25; // The compiler infers the type number for the variable age.
+
+let isValid = true; // The compiler infers the type boolean for the variable isValid
+
 // todo 👉 What are some best practices for using type inference in TypeScript?
 //? Use type inference for simple cases where the assigned value clearly indicates the intended type.
 //? When in doubt, provide explicit type annotations to make your intentions clear.
 //? Avoid relying too heavily on type inference when the assigned value is complex or ambiguous.
 //? Regularly review and refactor your code to ensure the inferred types align with your intentions.
 
+//! 🕵️‍♂️ Here are the two questions for practices 🕵️‍♂️ 
+// * 1: Declare a variable message and initialize it with the value "Hello, TypeScript!". Infer the type
+// * of message using type inference.
+
+
+
+
+// * 2: Write a function calculateArea that takes the length and width parameters of type number and 
+// * returns their product. Infer the return type of the function using type inference.
