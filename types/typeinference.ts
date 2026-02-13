802787035530e04a277e3d4817b23dd462ecd18a
unknown <muktinathrajbanshi786@gmail.com>
2026-02-13 10:03:15 +0545

    some noted are captured in this file

--- types/typeinference.ts
+++ types/typeinference.ts
@@ -4,4 +4,11 @@
 
 let myName = "muktinath";
 let myFavNum = 5;
-// myFavNum = true 
+// myFavNum = true
+
+// todo 👉 What are some best practices for using type inference in TypeScript?
+//? Use type inference for simple cases where the assigned value clearly indicates the intended type.
+//? When in doubt, provide explicit type annotations to make your intentions clear.
+//? Avoid relying too heavily on type inference when the assigned value is complex or ambiguous.
+//? Regularly review and refactor your code to ensure the inferred types align with your intentions.
+
