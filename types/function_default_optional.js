08b2868350ef51d2f67b33173b40d12f83f8aae9
unknown <muktinathrajbanshi786@gmail.com>
2026-02-13 10:45:55 +0545

    this example will be perfect for default and optional practice

--- types/function_default_optional.js
+++ types/function_default_optional.js
@@ -1,16 +1,9 @@
 //? Optional and Default Parameters:
-
 // * TypeScript allows you to define optional and default parameters in functions.
 // * Optional parameters are denoted by appending a ? symbol after the parameter name, and default
 // * parameters are specified by providing a default value in the parameter declaration.
-
-const greet3 = (name:string, id:number): string => {
-
-    return `Welcome, ${name} and your id is ${id}`
-
-}
-
-const employee = greet3("muktinath", 5);
+var greet3 = function (name, id) {
+    return "Welcome, ".concat(name, " and your id is ").concat(id);
+};
+var employee = greet3("muktinath", 5);
 console.log(employee);
-
-
