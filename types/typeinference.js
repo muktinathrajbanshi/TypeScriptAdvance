7666005fabbb958527dbf5f21f697e64f0ad17b6
unknown <muktinathrajbanshi786@gmail.com>
2026-02-13 09:43:03 +0545

    now let's get started

---
+++ types/typeinference.js
@@ -0,0 +1,6 @@
+// * Type inference in TypeScript refers to the ability of the TypeScript compiler to automatically
+// * determine and assign types to variables, expressions, and function return values based on their
+// * usage and context in the code.
+var myName = "muktinath";
+var myFavNum = 5;
+// myFavNum = true 
---
+++ types/typeinference.ts
@@ -0,0 +1,7 @@
+// * Type inference in TypeScript refers to the ability of the TypeScript compiler to automatically
+// * determine and assign types to variables, expressions, and function return values based on their
+// * usage and context in the code.
+
+let myName = "muktinath";
+let myFavNum = 5;
+// myFavNum = true 
