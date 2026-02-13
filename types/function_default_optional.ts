cf0dca49eca59bf2afb1adbe3b14e8e4ab860bd7
unknown <muktinathrajbanshi786@gmail.com>
2026-02-13 10:28:21 +0545

    Now let's get started with optional and default parameters

---
+++ types/function_default_optional.ts
@@ -0,0 +1,6 @@
+//? Optional and Default Parameters:
+
+// * TypeScript allows you to define optional and default parameters in functions.
+// * Optional parameters are denoted by appending a ? symbol after the parameter name, and default
+// * parameters are specified by providing a default value in the parameter declaration.
+
