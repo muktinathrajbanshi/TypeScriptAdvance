ce3de8741b43a5f2eb8f4e7b5b3ae8c5262a8621
unknown <muktinathrajbanshi786@gmail.com>
2026-02-14 07:45:03 +0545

    wow push pop method is seen

--- src/array_operations.js
+++ src/array_operations.js
@@ -1,22 +1,13 @@
 // * Arrays in TypeScript come with built-in methods that allow you to perform common operations
-
-const fruits: string[] = ["apple", "banana", "orange", "mango"];
-
+var fruits = ["apple", "banana", "orange", "mango"];
 // a) Adding elements to an array using push:
-
-const newUpdatedFruits = fruits.push("kiwi");
+var newUpdatedFruits = fruits.push("kiwi");
 // console.log(newUpdatedFruits);
 // console.log(fruits);
-
-
 // b) Removing elements from an array using pop:
-const lastData = fruits.pop();
+var lastData = fruits.pop();
 console.log(lastData);
 console.log(fruits);
-
-
-
-
 //! Iterating over elements:
 //? You can iterate over the elements of an array using various looping constructs such as for
 //? for...of loop
