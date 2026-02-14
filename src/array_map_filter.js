64bd3060c047ce1db62f819c9186112298f4285d
unknown <muktinathrajbanshi786@gmail.com>
2026-02-14 09:51:46 +0545

    wow map challenges is completed

--- src/array_map_filter.js
+++ src/array_map_filter.js
@@ -1,5 +1,5 @@
 // * Map & Filter Method in Array in TS
-var numbers = [1, 2, 3, 4, 5];
+// const numbers: number[] = [1, 2, 3, 4, 5];
 //! Map Method
 //* The map method creates a new array by applying a provided function to each element of the
 //* original array. It transforms each element and returns a new array with the transformed values.
@@ -14,15 +14,21 @@ var numbers = [1, 2, 3, 4, 5];
 // const evenNumbers:number[] = numbers.filter((curElem:number) => curElem%2 === 0 );
 // console.log(evenNumbers);
 //? 2: Filtering numbers greater than 3
-var numGreaterThanThree = numbers.filter(function (curElem) { return curElem > 3; });
-console.log(numGreaterThanThree);
+// const numGreaterThanThree:number[] = numbers.filter((curElem:number) => curElem > 3);
+// console.log(numGreaterThanThree);
 //todo Homework Time
 // * Practice questions for map:
 //? 1: Given an array of strings representing names, create a new array that contains the uppercase
 //? version of each name.
+// const names: string[] = ["Hari", "Ram", "Mukti", "Shyam", "Krishna"];
+// const newArray:string[] = names.map((curElem:string) => curElem.toUpperCase());
+// console.log(newArray);
 //? 2: Given an array of numbers, create a new array that contains the square of each number
+var numbers = [1, 2, 3, 4, 5];
+var squareOfEachNum = numbers.map(function (curNum) { return curNum * curNum; });
+console.log(squareOfEachNum);
 // * Practice questions for filter:
 //? 1: Given an array of strings, create a new array that contains only the strings with a length
-//? greater than 15.
+//? greater than 4.
 // * const names: string[] = ["Hari", "Ram", "Mukti", "Shyam", "Krishna"];
-//? 2: Given an array of strings, filter out the names
+//? 2: Given an array of strings, filter out the names that start with letter "A".
--- src/array_map_filter.ts
+++ src/array_map_filter.ts
@@ -1,6 +1,6 @@
 // * Map & Filter Method in Array in TS
 
-const numbers: number[] = [1, 2, 3, 4, 5];
+// const numbers: number[] = [1, 2, 3, 4, 5];
 
 //! Map Method
 //* The map method creates a new array by applying a provided function to each element of the
@@ -29,21 +29,36 @@ const numbers: number[] = [1, 2, 3, 4, 5];
 
 //? 2: Filtering numbers greater than 3
 
-const numGreaterThanThree:number[] = numbers.filter((curElem:number) => curElem > 3);
+// const numGreaterThanThree:number[] = numbers.filter((curElem:number) => curElem > 3);
 
-console.log(numGreaterThanThree);
+// console.log(numGreaterThanThree);
 
 
 //todo Homework Time
 // * Practice questions for map:
+
 //? 1: Given an array of strings representing names, create a new array that contains the uppercase
 //? version of each name.
 
+// const names: string[] = ["Hari", "Ram", "Mukti", "Shyam", "Krishna"];
+
+// const newArray:string[] = names.map((curElem:string) => curElem.toUpperCase());
+
+// console.log(newArray);
+
+
 //? 2: Given an array of numbers, create a new array that contains the square of each number
+
+const numbers: number[] = [1, 2, 3, 4, 5];
+
+const squareOfEachNum:number[] = numbers.map((curNum:number) => curNum * curNum);
+
+console.log(squareOfEachNum);
+
 
 // * Practice questions for filter:
 //? 1: Given an array of strings, create a new array that contains only the strings with a length
-//? greater than 15.
+//? greater than 4.
 
 // * const names: string[] = ["Hari", "Ram", "Mukti", "Shyam", "Krishna"];
 //? 2: Given an array of strings, filter out the names that start with letter "A".
