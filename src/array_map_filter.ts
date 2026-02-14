a21ac175c6d4fb3ab53c662a012585eea3644b6c
unknown <muktinathrajbanshi786@gmail.com>
2026-02-14 09:18:46 +0545

    now let's get started with array map and filter methods

---
+++ src/array_map_filter.ts
@@ -0,0 +1,26 @@
+// * Map & Filter Method in Array in TS
+
+const numbers: number[] = [1, 2, 3, 4, 5];
+
+//! Map Method
+//? 1: Doubling each number
+//? 2: Converting numbers to strings
+
+
+//! Filter Method
+//? 1: Filtering even numbers
+//? 2: Filtering numbers greater than 3
+
+//todo Homework Time
+// * Practice questions for map:
+//? 1: Given an array of strings representing names, create a new array that contains the uppercase
+//? version of each name.
+
+//? 2: Given an array of numbers, create a new array that contains the square of each number
+
+// * Practice questions for filter:
+//? 1: Given an array of strings, create a new array that contains only the strings with a length
+//? greater than 15.
+
+// * const names: string[] = ["Hari", "Ram", "Mukti", "Shyam", "Krishna"];
+//? 2: Given an array of strings, filter out the names
