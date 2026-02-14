31500dafb5e0bef7391ff08d6cae4f47b1922d44
unknown <muktinathrajbanshi786@gmail.com>
2026-02-14 09:32:51 +0545

    wow map method is completed re baba

--- src/array_map_filter.js
+++ src/array_map_filter.js
@@ -1,38 +1,24 @@
 // * Map & Filter Method in Array in TS
-
-const numbers: number[] = [1, 2, 3, 4, 5];
-
+var numbers = [1, 2, 3, 4, 5];
 //! Map Method
 //* The map method creates a new array by applying a provided function to each element of the
 //* original array. It transforms each element and returns a new array with the transformed values.
-
 //? 1: Doubling each number
-
 // const doubleData:number[] = numbers.map((curVal:number) => curVal * 2 );
 // console.log(doubleData);
-
 //? 2: Converting numbers to strings
-
-const numberToString:string[] = numbers.map((curVal:number) => curVal.toString() );
-
+var numberToString = numbers.map(function (curVal) { return curVal.toString(); });
 console.log(numberToString);
-
-
-
 //! Filter Method
 //? 1: Filtering even numbers
 //? 2: Filtering numbers greater than 3
-
 //todo Homework Time
 // * Practice questions for map:
 //? 1: Given an array of strings representing names, create a new array that contains the uppercase
 //? version of each name.
-
 //? 2: Given an array of numbers, create a new array that contains the square of each number
-
 // * Practice questions for filter:
 //? 1: Given an array of strings, create a new array that contains only the strings with a length
 //? greater than 15.
-
 // * const names: string[] = ["Hari", "Ram", "Mukti", "Shyam", "Krishna"];
 //? 2: Given an array of strings, filter out the names
