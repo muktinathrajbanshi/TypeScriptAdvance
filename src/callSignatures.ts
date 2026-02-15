9ace5b13ff0730749cc3a883cb9c90466b13d395
unknown <muktinathrajbanshi786@gmail.com>
2026-02-15 11:06:56 +0545

    now let's focus on call signature

---
+++ src/callSignatures.ts
@@ -0,0 +1,10 @@
+// * The function call signature refers to the declaration or definition of a function, which includes
+// * the function's name, parameters, and return type. It defines the structure and type information
+// * of a function without including the function's implementation or body.
+
+//? lets add one function inside the object. It's more like creating a method in object
+
+//? Very Important => call signatures are typically used inside object type notations to describe the
+//? shape of functions within object types.
+
+//! Homework
