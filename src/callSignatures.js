1de8870684f659b9dc766c6a253e3bb641193884
unknown <muktinathrajbanshi786@gmail.com>
2026-02-15 20:06:53 +0545

    Call Signatures in TypeScript with Real-life Examples & Best Practices

--- src/callSignatures.js
+++ src/callSignatures.js
@@ -4,10 +4,18 @@
 var student1 = {
     name: "Muktinath Rajbanshi",
     age: 23,
+    greet: function (country) { return "Welcome My name is ".concat(student1.name, ", I am ").concat(student1.age, " years old and I am from ").concat(country); }
+};
+var student2 = {
+    name: "Milan Rajbanshi",
+    age: 24,
+    greet: function (country) { return "Welcome My name is ".concat(student2.name, ", I am ").concat(student2.age, " years old and I am from ").concat(country); }
 };
 var introduction = function (student1) {
     var name = student1.name, age = student1.age;
     return "Welcome My name is ".concat(name, ", I am ").concat(age, " years old");
 };
-console.log(introduction(student1));
+// console.log(introduction(student1));
+console.log(student1.greet("Nepal"));
+console.log(student2.greet("India"));
 //! Homework
--- src/callSignatures.ts
+++ src/callSignatures.ts
@@ -11,20 +11,31 @@ type Student = {
     name : string;
     age : number;
     gender? : string;
+    greet : (country : string) => string // method call signature
+    // (country : string) : string // call signature
 }
 
 const student1 : Student = {
     name : "Muktinath Rajbanshi",
     age : 23,
+    greet : (country) => `Welcome My name is ${student1.name}, I am ${student1.age} years old and I am from ${country}`
 }
 
+const student2 : Student = {
+    name : "Milan Rajbanshi",
+    age : 24,
+    greet : (country) => `Welcome My name is ${student2.name}, I am ${student2.age} years old and I am from ${country}`
+}
 
 const introduction : (student1 : Student) => string = (student1 : Student) : string => {
     const { name, age } = student1;
     return `Welcome My name is ${name}, I am ${age} years old`
 }
 
-console.log(introduction(student1));
+// console.log(introduction(student1));
+console.log(student1.greet("Nepal"));
+console.log(student2.greet("India"));
+
 
 
 //! Homework
