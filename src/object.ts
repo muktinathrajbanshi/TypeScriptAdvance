57517e727d568b254c8c053cca551f451073e523
unknown <muktinathrajbanshi786@gmail.com>
2026-02-15 08:30:51 +0545

    let's get started with object in TypeScript

---
+++ src/object.ts
@@ -0,0 +1,19 @@
+// * In TypeScript, objects are used to represent data with key-value pairs. Each key in the object
+// * is a string (or a symbol in ES6) that maps to a value
+
+//? let's consider a real-life example of representing a person's information using TypeScript 
+//? objects
+//? 🙅‍♂️ -> name, age, isStudent, address{ city, country }
+
+
+// Todo Accessing the object data
+// Todo Updating object properties
+
+//! Homework
+//? Create a Product Object:
+//? Define a product with properties for name, price, and quantity. Create a product object with 
+//? the following data:
+
+// Name : "Laptop"
+// Price : 1000
+// Quantity : 5
