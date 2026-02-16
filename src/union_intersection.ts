e92c563522b392519c55113f73b9f7862ecd5294
unknown <muktinathrajbanshi786@gmail.com>
2026-02-16 13:57:30 +0545

    aja lai yati la

---
+++ src/union_intersection.ts
@@ -0,0 +1,15 @@
+//? Union types allow you to specify that a variable can hold values of multiple types. You use the 
+//? | (pipe) symbol to define a union type.In
+
+// * In TypeScript, when using a union type, it is essential to ensure that at least one of the types in
+// * the union includes all the required properties. Failure to do so will result in a type error during
+// * compilation.
+
+
+
+
+// * Practice, we'll create a function that can accept different types of arguments and perform different
+// * actions based on the type of the input. Suppose we want to create a function that doubles the value
+// * if the input is a number, or converts the input to uppercase if it's a string. To achieve this, we
+// * can use a union type to allow the function to accept both numbers and strings.
+
