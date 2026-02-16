63d4be8e927a5910aba1897207b1e4b9a04f2870
unknown <muktinathrajbanshi786@gmail.com>
2026-02-16 07:48:33 +0545

    now let's get started with tuples in TS

---
+++ src/tuples.ts
@@ -0,0 +1,10 @@
+// * In TypeScript, tuples are a data structure that allows you to store a fixed-size collection of
+// * elements of different types. They are similar to arrays, but with a key difference: the types of
+// * elements in a tuple are fixed and declared at the time of creation, whereas arrays can hold elements
+// * of the same type, and their size can vary.
+
+//? Real-life example of using tuples in TypeScript:
+
+//? Let's consider a scenario where you want to represent a person's basic information, including their
+//? name, age, and whether they have a driver's license. Using a tuple can be an appropriate choice 
+//? because these three elements have a specific order and type.
