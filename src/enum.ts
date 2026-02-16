19095ebce79e8a0c3c25ad23a3516811da50eb8f
unknown <muktinathrajbanshi786@gmail.com>
2026-02-16 07:08:54 +0545

    Now let's get started

---
+++ src/enum.ts
@@ -0,0 +1,12 @@
+// * ----------------------------------
+// * Enums in TS
+// * ----------------------------------
+
+//? Enums in TypeScript are commonly used when you want to represent a set of related values and choose
+//? one value from multiple options. Enums provide a convenient way to define a set of named values and
+//? associate them with specific meanings.
+
+
+// * In TypeScript, when enum constants are not explicitly assigned numeric values, they are 
+// * automatically assigned incremental numeric values starting from 0. The default numeric value for
+// * the first enum constant is 0, and subsequent enum constants receive values incremented by 1.
