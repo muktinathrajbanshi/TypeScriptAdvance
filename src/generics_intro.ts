e200552d7ebc35171f1e1760d4e36661ab56dcb3
unknown <muktinathrajbanshi786@gmail.com>
2026-02-18 07:42:26 +0545

    wah kiya baat hain

--- src/generics_intro.ts
+++ src/generics_intro.ts
@@ -1,19 +1,34 @@
 //* Generics in TypeScript allow you to create reusable components or functions that can work with
 //* multiple data types.
 
-function logAndReturn(value : number | string): number | string {
-    console.log( value);
+// function logAndReturn(value : number | string): number | string {
+//     console.log( value);
+//     return value;
+    
+// }
+
+
+// const numberResult : string | number = logAndReturn(42);
+// const stringResult : string | number = logAndReturn("Hello, Generics");
+// const booleanResult  = logAndReturn(true);
+
+// console.log(numberResult);
+// console.log(stringResult);
+// console.log(booleanResult);
+
+
+//! Generic function to log and return an input value
+function logAndReturn<T>(value : T) : T {
+    console.log(value);
     return value;
     
 }
 
-
-const numberResult : string | number = logAndReturn(42);
-const stringResult : string | number = logAndReturn("Hello, Generics");
-const booleanResult  = logAndReturn(true);
+//! Using the generic function with different types
+const numberResult : number = logAndReturn<number>(42);
+const stringResult : string = logAndReturn<string>("Hello, Generics");
+const booleanResult : boolean = logAndReturn<boolean>(true);
 
 console.log(numberResult);
 console.log(stringResult);
 console.log(booleanResult);
-
-
