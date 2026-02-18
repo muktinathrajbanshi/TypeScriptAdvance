17442bb32e7627b8828b6b454aad79f79d621b96
unknown <muktinathrajbanshi786@gmail.com>
2026-02-18 19:02:41 +0545

    oh my god let's started with interface

---
+++ src/interface.ts
@@ -0,0 +1,14 @@
+//* In TypeScript, an interface is a powerful feature that allows you to define a contract for an 
+//* object's shape. It specifies the properties and their types that an object must have to be considered
+//* of that particular interface type. Interfaces are primarily used for type-checking during development 
+//* do not generate any JavaScript code at runtime
+
+
+
+
+//todo Create a Product Object:
+//? Define an interface or type representing a product with properties for name, price, and quantity.
+//? Create a product object with the following data:
+//* Name : "Laptop"
+//* Price : 1000
+//* Quantity: 5
