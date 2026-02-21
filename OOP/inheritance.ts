a3d90d67817b369effd6d3d982e9ad66ce18ee52
unknown <muktinathrajbanshi786@gmail.com>
2026-02-21 16:01:03 +0545

    haha it happens sometimes but let it go

---
+++ OOP/inheritance.ts
@@ -0,0 +1,7 @@
+// ****************************
+//* Inheritance in TS
+//* ***************************
+
+//* Inheritance allows a class to reuse the functionality of an existing class without rewriting it.
+//? Inheritance is a mechanism in which one class acquires the property of another class. For example,
+//? a child inherits the traits of his/her parents.
