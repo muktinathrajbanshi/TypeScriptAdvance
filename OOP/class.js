e43f1c6973a4a26c892aa557cacbe4645bed7166
unknown <muktinathrajbanshi786@gmail.com>
2026-02-21 14:47:04 +0545

    wow what a example haha

--- OOP/class.js
+++ OOP/class.js
@@ -1,20 +1,18 @@
 // ************************************
 //* How to create a class and also instance of class in TS
 //**************************************
-
 //* A class in terms of OOP is a blueprint for creating objects.
 //* A class is like a blueprint for creating similar things.
-
 //? In TypeScript, there is a convention to use PascalCase (also known as UpperCamelCase) for
 //? class names. This means that class names should start with a capital letter and each
 //? subsequent word in the class name should also begin with a capital letter.
-
-
-class Persons {
-    name : string = "Muktinath Rajbanshi";
-    age : number = 14;
-    hobbies : string[] = ["reading", "painting"];
-}
-
-const person1 : Persons = new Persons();
+var Persons = /** @class */ (function () {
+    function Persons() {
+        this.name = "Muktinath Rajbanshi";
+        this.age = 14;
+        this.hobbies = ["reading", "painting"];
+    }
+    return Persons;
+}());
+var person1 = new Persons();
 console.log(person1.hobbies);
