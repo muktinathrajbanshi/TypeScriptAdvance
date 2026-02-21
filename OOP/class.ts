07ecd5e2114b7bdc28144d6d822484bf7d127131
unknown <muktinathrajbanshi786@gmail.com>
2026-02-21 12:59:03 +0545

    Let's get started with OOP in TS

---
+++ OOP/class.ts
@@ -0,0 +1,10 @@
+// ************************************
+//* How to create a class and also instance of class in TS
+//**************************************
+
+//* A class in terms of OOP is a blueprint for creating objects.
+//* A class is like a blueprint for creating similar things.
+
+//? In TypeScript, there is a convention to use PascalCase (also known as UpperCamelCase) for
+//? class names. This means that class names should start with a capital letter and each
+//? subsequent word in the class name should also begin with a capital letter.
