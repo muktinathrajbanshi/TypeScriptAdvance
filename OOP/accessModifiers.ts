1df52f0e33648132dd77295ec06c971f6b37e93e
unknown <muktinathrajbanshi786@gmail.com>
2026-02-21 21:01:44 +0545

    wow what a example man

--- OOP/accessModifiers.ts
+++ OOP/accessModifiers.ts
@@ -4,3 +4,47 @@
 // protected	       ✅ Yes	✅ Yes	❌ No
 // private	           ✅ Yes	❌ No	❌ No
 
+
+class Persons {
+    public name : string;
+    protected age : number;
+    hobbies : string[];
+
+
+    constructor(name:string, age:number, hobbies:string[]) {
+        this.name = name;
+        this.age = age;
+        this.hobbies = hobbies;
+    }
+
+    introduce(): string{
+        return `Hi, I'm ${this.name} and I'm ${this.age} years old. I love ${this.hobbies.join(", ")}.`
+    }
+
+}
+
+class Student extends Persons {
+    grade : number;
+
+    constructor(name:string, age:number, hobbies:string[], grade : number) {
+        super(name, age, hobbies);
+        this.grade = grade;
+    }
+
+    introduce(): string{
+        return `${super.introduce()}. I am in grade ${this.grade}`;
+    }
+}
+
+const person1 : Persons = new Persons("muktinath", 14, ["reading", "painting"]);
+const person2 : Persons = new Persons("rajbanshi", 15, ["coding", "sports"]);
+
+
+console.log(person1.name);
+
+
+
+const student : Persons = new Student("mukti", 16, ["singing", "coding"], 10);
+
+
+console.log(student.introduce());
