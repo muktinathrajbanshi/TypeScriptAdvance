8ae01c3f3311acedbfb094a422befea06390e420
unknown <muktinathrajbanshi786@gmail.com>
2026-02-21 20:48:33 +0545

    now let's focus on access modifiers

---
+++ OOP/accessModifiers.ts
@@ -0,0 +1,6 @@
+// 🔐 Access Modifiers Relationship Table
+// Access Modifier	Parent Class	Child Class (Subclass)	Outside Class
+// public	           ✅ Yes	✅ Yes	✅ Yes
+// protected	       ✅ Yes	✅ Yes	❌ No
+// private	           ✅ Yes	❌ No	❌ No
+
