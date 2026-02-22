e7cd5a1c379e00b338e46c7db59571d7082e4003
unknown <muktinathrajbanshi786@gmail.com>
2026-02-22 20:51:16 +0545

    next question is added yar

--- src/getter_setter.ts
+++ src/getter_setter.ts
@@ -17,3 +17,15 @@
 //* Instantiate an object of the BankAccount class.
 //* Use the setter to set the balance to 1000 and use the getter to display the updated balance.
 //* Try setting a negative balance using the setter. What output do you expect?
+
+
+//? Q2: Temperature Converter
+//* Define a TypeScript class Temperature with a private property _celsius set to 0.
+//* Implement a getter method celsius that returns the temperature in Celsius.
+//* Implement a setter method celsius that sets the temperature in Celsius.
+//* Implement a getter method fahrenheit that converts Celsius to Fahrenheit using the formula (C * 9/5) + 32.
+//* Implement a getter method fahrenheit that converts Fahrenheit to Celsius using the formula (F - 32) * 5/9.
+//* Create an instance of the Temperature class.
+//* Use the setter to set the temperature in Celsius to 25 and then use the getter for Fahrenheit. What
+//* Fahrenheit value do you expect?
+//* Use the setter to set the temperature in Fahrenheit to 98.6 and then use the getter for Celsius.
