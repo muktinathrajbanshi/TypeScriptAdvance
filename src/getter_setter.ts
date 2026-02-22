b5bead998d5cf8f95c32247de3da415367abdcdd
unknown <muktinathrajbanshi786@gmail.com>
2026-02-22 20:33:50 +0545

    now let's get started with getter and setter

---
+++ src/getter_setter.ts
@@ -0,0 +1,10 @@
+//* ********************
+//* GETTER & SETTER
+//* ********************
+
+//? In TypeScript classes, you can use getter and setter methods to control the access and modification
+//? of class properties. Getter methods allow you to retrieve the value of a property, while setter 
+//? methods allow you to set the value of a property with additional logic or validation.
+
+//TODO The get method doesn't take any parameters, and the set method takes only one parameter.
+
