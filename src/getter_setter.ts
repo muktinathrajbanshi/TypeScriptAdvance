d404bcd1659d07410af3cecd0285b21a9f143d07
unknown <muktinathrajbanshi786@gmail.com>
2026-02-22 20:40:48 +0545

    for practice purpose i write some question now let's solve it ok

--- src/getter_setter.ts
+++ src/getter_setter.ts
@@ -8,3 +8,12 @@
 
 //TODO The get method doesn't take any parameters, and the set method takes only one parameter.
 
+//* PRACTICE TIME
+//? Q1: Bank Account Balance
+//* Create a TypeScript class BankAccount with a private property _balance initialized to 0.
+//* Implement a getter method balance that returns the current balance.
+//* Implement a setter method balance that updates the balance if the new value is non-negative. Otherwise,
+//* log an error message.
+//* Instantiate an object of the BankAccount class.
+//* Use the setter to set the balance to 1000 and use the getter to display the updated balance.
+//* Try setting a negative balance using the setter. What output do you expect?
