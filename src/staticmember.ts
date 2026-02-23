0a866d8279739f6f62f35d3d321a9b62a9c46675
unknown <muktinathrajbanshi786@gmail.com>
2026-02-23 20:27:01 +0545

    let's go now with staticmembers in TS

---
+++ src/staticmember.ts
@@ -0,0 +1,11 @@
+// ********************************
+//* STATIC PROPERTIES & METHODS
+// ********************************
+
+//* In TypeScript, static methods and properties belong to the class itself rather than to instance
+//* of the class. By making methods and properties static, we can access them directly from the class
+//* without needing to create an instance of the class. This is useful for utility functions or properties
+//* that don't rely on instance-specific data.
+
+//? Example: Math Operations Utility - creating a utility class to perform various methematical operations.
+
