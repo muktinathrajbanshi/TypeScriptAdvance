0bf001e64b22e4bc8613682f700198fe6c5306a1
unknown <muktinathrajbanshi786@gmail.com>
2026-02-23 21:22:49 +0545

    are wah

--- src/staticmember.js
+++ src/staticmember.js
@@ -1,23 +1,19 @@
 // ********************************
 //* STATIC PROPERTIES & METHODS
 // ********************************
-
 //* In TypeScript, static methods and properties belong to the class itself rather than to instance
 //* of the class. By making methods and properties static, we can access them directly from the class
 //* without needing to create an instance of the class. This is useful for utility functions or properties
 //* that don't rely on instance-specific data.
-
 //? Example: Math Operations Utility - creating a utility class to perform various methematical operations.
-
 // class Person {
 //     constructor(public name : string) {}
 // }
-
 // const user1 = new Person("muktinath");
-
-class MathOperations {
-    public static PI : number = Math.PI;
-}
-
+var MathOperations = /** @class */ (function () {
+    function MathOperations() {
+    }
+    MathOperations.PI = Math.PI;
+    return MathOperations;
+}());
 console.log(MathOperations);
-
