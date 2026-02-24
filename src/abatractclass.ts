ed7e4baf52d5b4bd23932d9a2121dcdc8e482ebf
unknown <muktinathrajbanshi786@gmail.com>
2026-02-24 20:02:32 +0545

    aba bistarai bistarai saknu paro

--- src/abatractclass.ts
+++ src/abatractclass.ts
@@ -1,3 +1,7 @@
 //* Abstract classes provide a way to define common properties and methods that multiple derived
 //* classes can share. This promotes code reuse and helps establish a common interface for related
 //* classes. Abstract class cannot be instantiated
+
+//* Abstract classes focus on class inheritance and sharing common functionality, whereas the
+//* useContext hook in React focuses on managing global state and allowing components to consume that
+//* state.
