4ef7fdc545bb647c50a74e64a51339fddd3dbab4
unknown <muktinathrajbanshi786@gmail.com>
2026-02-24 19:59:08 +0545

    hey bhagban aja ta jhanai dhilo vayo yar

---
+++ src/abatractclass.ts
@@ -0,0 +1,3 @@
+//* Abstract classes provide a way to define common properties and methods that multiple derived
+//* classes can share. This promotes code reuse and helps establish a common interface for related
+//* classes. Abstract class cannot be instantiated
