2034cfc4cb984e530b8574942cc1d0e673621b6a
unknown <muktinathrajbanshi786@gmail.com>
2026-02-25 08:18:43 +0545

    Now let's focus on difference between type and interface

---
+++ src/type_interface.ts
@@ -0,0 +1,14 @@
+//* *************************
+//* INTERFACE VS TYPE
+//************************** *
+
+// 1:
+//? Use custom types when you need unions, intersections, or mapped types.
+//? Use interfaces when defining object shapes or classes that adhere to a contract
+
+// 2:
+//? Interfaces can extend other interfaces to inherit their members.
+//? Custom types can use unions and intersections for more complex type compositions.
+
+//* same Interface name treated as one
+
