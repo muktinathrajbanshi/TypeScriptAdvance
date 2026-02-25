abaed0011a76b2fa22956f2e2e07899bb794ad11
unknown <muktinathrajbanshi786@gmail.com>
2026-02-25 12:17:50 +0545

    wow what a logic bro

--- src/typeguard.ts
+++ src/typeguard.ts
@@ -2,3 +2,17 @@
 //* In typeScript, type narrowing allows you to write type-safe code by ensuring you only operate on
 //* the correct type under certain circumstances. This is particularly useful with union types and 
 //* generic types.
+
+
+const favHobbies = (hobby: string | string[]) => {
+    // return hobby.map((curVal) => {
+    //     return curVal;
+    // })
+
+    if (typeof hobby === "object") {
+        return hobby.map(() => {});
+    } else {
+        console.log(hobby);
+        
+    }
+}
