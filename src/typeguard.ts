19363e21bdda1f5d65d879c77e360bb1207df68e
unknown <muktinathrajbanshi786@gmail.com>
2026-02-25 08:44:14 +0545

    now let's focus on typeguard in TS ok

---
+++ src/typeguard.ts
@@ -0,0 +1,4 @@
+//* A typeof guard in TypeScript lets you narrow down the type of a variable based on its runtime value.
+//* In typeScript, type narrowing allows you to write type-safe code by ensuring you only operate on
+//* the correct type under certain circumstances. This is particularly useful with union types and 
+//* generic types.
